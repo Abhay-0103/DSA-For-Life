b3b391b317735e3e800672cbce0b038fbed5052e
Abhay-0103 <ab0321054@gmail.com>
2026-05-05 13:37:38 +0530

    Pattern-2

---
+++ src/Pattern/Pattern_2.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class Pattern_2 {
+    static void main() {
+
+        int n =3;
+        int j= 5;
+
+        for (int row=1; row<=n; row++){
+            for (int col=1; col<=j; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
