5cde5a01e1f42398a0f5d410feea751e43007e7b
Abhay-0103 <ab0321054@gmail.com>
2026-05-06 21:53:55 +0530

    Mehtod

---
+++ src/Methods/Method_01.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Method_01 {
+    static void Print2KaTable() {
+        for (int i=1; i<=10; i++){
+            int ans = 2*i;
+            System.out.println("->" + ans);
+        }
+    }
+    static void main() {
+        System.out.println("Hello Jiiii");
+        Print2KaTable();
+        System.out.println("Chaloo Byeee..");
+    }
+}
---
+++ src/Methods/main.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class main {
+    static void main() {
+        PrintHello();
+    }
+    static void PrintHello() {
+        System.out.println("Hello Betta ...!");
+    }
+}
