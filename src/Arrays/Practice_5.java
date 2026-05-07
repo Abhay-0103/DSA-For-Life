ae3b552c7b42d673a2438d5ff6e93094d82aa746
Abhay-0103 <ab0321054@gmail.com>
2026-05-07 16:40:29 +0530

    2-D

---
+++ src/Arrays/Practice_5.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Practice_5 {
+    static void main() {
+        int[][] arr;
+        arr = new int[3][4];
+        int[][] brr = {
+                {1,2},
+                {3,4},
+                {5,6},
+                {7,8}
+        };
+        // System.out.println(brr[3][1]);
+        int rowLength = brr.length;
+        int colLength = brr[0].length;
+
+        for (int rowIndex = 0; rowIndex<=rowLength-1; rowIndex++){
+            for (int colIndex=0; colIndex<=colLength-1; colIndex++){
+                System.out.print(brr[rowIndex][colIndex] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Arrays/Practice_6.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Practice_6 {
+    static void main() {
+        int[][] arr;
+        arr = new int[3][4];
+        int[][] brr = {
+                {1,2},
+                {2,3,5,6},
+                {45,5},
+                {6}
+        };
+        int rowlenght = brr.length;
+
+        for (int rowIndex=0; rowIndex<=rowlenght-1; rowIndex++){
+            int colLenght = brr[rowIndex].length;
+            for (int colIndex=0; colIndex<=colLenght-1; colIndex++){
+                System.out.print(brr[rowIndex][colIndex] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
