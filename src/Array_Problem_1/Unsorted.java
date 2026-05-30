525b2726bae060b389e59e9b797f3de6fa9f8915
Abhay-0103 <ab0321054@gmail.com>
2026-05-30 23:09:40 +0530

    Finding Unsorted Array

---
+++ src/Array_Problem_1/Unsorted.java
@@ -0,0 +1,17 @@
+package Array_Problem_1;
+
+public class Unsorted {
+    static int getUnsortedElement(int arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i+1] <= arr[i]) {
+                return arr[i+1];
+            }
+        }
+        return -1;
+    }
+
+    static void main() {
+    int arr[] = {1,2,5,4,9};
+        System.out.println(getUnsortedElement(arr));
+    }
+}
